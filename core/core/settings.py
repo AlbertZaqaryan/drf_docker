8d65966c1d614e156bea7a6d4be51d571594d909
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2025-03-28 13:15:56 +0400

    v2

--- core/core/settings.py
+++ core/core/settings.py
@@ -85,17 +85,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': os.getenv('DB_NAME'),
-		'USER': os.getenv('DB_USER'),
-		'PASSWORD': os.getenv('DB_PASSWORD'),
-		'HOST':os.getenv('DB_HOST'),
-		'PORT':os.getenv('DB_PORT'),
-	}
-}
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': os.getenv('DB_NAME'),
+# 		'USER': os.getenv('DB_USER'),
+# 		'PASSWORD': os.getenv('DB_PASSWORD'),
+# 		'HOST':os.getenv('DB_HOST'),
+# 		'PORT':os.getenv('DB_PORT'),
+# 	}
+# }
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
